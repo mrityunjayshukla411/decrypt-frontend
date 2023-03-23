bf637bc106139a6dc9bcfcf5306da3a0ded1e75b
Mrityunjay Shukla <69010998+mrityunjayshukla411@users.noreply.github.com>
2023-03-24 00:31:44 +0530

    Update SignUp.js

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -40,7 +40,7 @@ function SignUp() {
   useEffect(async () => {
 
     const jwt = JSON.parse(localStorage.getItem("jwt"));
-    await axios.get("http://localhost:4000/check-verified", {
+    await axios.get("https://d3crypt-backend.onrender.com/check-verified", {
       headers: {
         'x-access-token': jwt
       }
